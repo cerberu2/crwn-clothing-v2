88b7b76b3c5f394980152e0a4bde2699cd1973e6
Borges <luisfvborges@gmail.com>
2026-01-19 17:47:17 +0000

    updating our directory page with directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
+import './categories.styles.scss';
+import CategoryDirectory from './components/category-directory/category-directory.component'; 
+
 const App = () => {
+
+   const categories = [
+        {
+            title: 'Hats',
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+            id: 1,
+            linkUrl: 'shop/hats',
+        },
+        {
+            title: 'Jackets',
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+            id: 2,
+            linkUrl: 'shop/jackets',
+        },
+        {
+            title: 'Sneakers',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            id: 3,
+            linkUrl: 'shop/sneakers',
+        },
+        {
+            title: 'Womens',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            id: 4,
+            linkUrl: 'shop/womens',
+        },
+        {
+            title: 'Mens',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+            id: 5,
+            linkUrl: 'shop/mens',
+        },
+    ];
+
+
   return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
+    <CategoryDirectory categories={categories}/>
   );
 };
 
